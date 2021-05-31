b46f3af6af26892d97d790ab6907281253732b34
Redon Xharja <redon.xharja@gmail.com>
2021-05-31 19:00:51 -0400

    added spicetify data

---
+++ spicetify_data/Extracted/Raw/xpui/xpui-routes-create-playlist.js
@@ -0,0 +1,2 @@
+(("undefined"!=typeof self?self:global).webpackChunkopen=("undefined"!=typeof self?self:global).webpackChunkopen||[]).push([[985],{22304:function(e,n,l){"use strict";l.r(n),l.d(n,{CreatePlaylist:function(){return p}});var t=l(27378),u=l.n(t),f=l(48529),s=l(79308),a=l(18010),o=l(20258),c=l(35181);const p=u().memo((()=>{const e=(0,s.I0)(),n=(0,f.k6)(),l=(0,o.P)();return(0,t.useEffect)((()=>{null!==l&&e((0,a.cg)(c.ag.get("playlist.new-default-name",l),(({url:e})=>{n.push(e)})))}),[e,n,l]),null}));n.default=p}}]);
+//# sourceMappingURL=xpui-routes-create-playlist.js.map
